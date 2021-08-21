a97707654748aa30506f9aeecd5d8d235f7165ff
ground7 <ground7@gmail.com>
2021-08-20 23:39:34 -0400

    Update collector and make prometheus config

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # opensea-exporter
 Prometheus exporter for opensea
+
+To run:
+
+```bash
+docker run --name opensea-exporter -p 9000:9000 --restart=unless-stopped opensea-exporter:latest
+docker run --name prometheus -p 9090:9090 -v /home/josiah/git-repos/opensea-exporter/prometheus.yaml:/etc/prometheus/prometheus.yml prom/prometheus
+```
--- collector.py
+++ collector.py
@@ -9,19 +9,22 @@ app = Flask(__name__)
 graphs = {}
 graphs['eth'] = Gauge('opensea_eth_value', 'The current value of ethereum according to OpenSea')
 
-# Go to the root domain to request the eth price from opensea (will set up a timer or something later)
-# Root is at http://localhost:9000/
 @app.route("/")
 def process_request():
+    url = "https://api.opensea.io/api/v1/events"
+    querystring = {"asset_contract_address":"0xc36cf0cfcb5d905b8b513860db0cfe63f6cf9f5c","token_id":"46958966635089507957945695825584013180928","event_type":"successful","only_opensea":"false","offset":"0","limit":"50"}
+    headers = {"Accept": "application/json"}
+    event_response = requests.request("GET", url, headers=headers, params=querystring)
     url = "https://api.opensea.io/api/v1/asset/0xc36cf0cfcb5d905b8b513860db0cfe63f6cf9f5c/46958966635089507957945695825584013180928/"
     response = requests.request("GET", url)
     eth_price = response.json()["collection"]["payment_tokens"][0]["usd_price"]
     graphs['eth'].set(eth_price)   # Set to current value of eth in usd
-    return str(eth_price)
+    return str(event_response.text)
 
 # Go to http://localhost:9000/metrics to see the prometheus endpoint
 @app.route("/metrics")
 def requests_count():
+    process_request()
     res = []
     for k,v in graphs.items():
         res.append(prometheus_client.generate_latest(v))
